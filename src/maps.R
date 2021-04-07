6fe79c9227e7df700fb942773daa762bbe5462cf
thisismactan <mactan@gmail.com>
2021-04-07 13:56:00 -0400

    added detailed infobox to state map

--- src/maps.R
+++ src/maps.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(scales)
 library(rgdal)
 library(rmapshaper)
 library(sp)
@@ -13,20 +14,127 @@ state_summary_stats <- state_sims %>%
   group_by(state, sim_id) %>%
   mutate(state_total_seats = sum(total_seats)) %>%
   group_by(state, party) %>%
-  summarise(avg_pct = mean(pct),
-            avg_seats = mean(total_seats),
-            avg_seat_pct = mean(total_seats / state_total_seats))
+  summarise(vote_pct05 = quantile(pct, 0.05),
+            vote_pct50 = median(pct),
+            vote_pct95 = quantile(pct, 0.95),
+            seats_pct05 = quantile(total_seats, 0.05),
+            seats_pct50 = median(total_seats),
+            seats_pct95 = quantile(total_seats, 0.95))
 
-state_summary_stats_wide <- state_sims %>%
-  group_by(state, sim_id) %>%
-  mutate(state_total_seats = sum(total_seats)) %>%
-  group_by(state, party)
-  
-
-state_largest_party <- state_summary_stats %>%
+# Separate data frames for largest party, second largest party, and so on
+state_summary_stats_1 <- state_summary_stats %>%
   group_by(state) %>%
-  filter(avg_pct == max(avg_pct)) %>%
-  dplyr::select(state, largest_party = party, largest_pct = avg_pct, largest_seats = avg_seats, largest_seat_pct = avg_seat_pct)
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(1) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_1"), .cols = -state)
+
+state_summary_stats_2 <- state_summary_stats %>%
+  group_by(state) %>%
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(2) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_2"), .cols = -state)
+
+state_summary_stats_3 <- state_summary_stats %>%
+  group_by(state) %>%
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(3) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_3"), .cols = -state)
+
+state_summary_stats_4 <- state_summary_stats %>%
+  group_by(state) %>%
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(4) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_4"), .cols = -state)
+
+state_summary_stats_5 <- state_summary_stats %>%
+  group_by(state) %>%
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(5) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_5"), .cols = -state)
+
+state_summary_stats_6 <- state_summary_stats %>%
+  group_by(state) %>%
+  arrange(state, desc(vote_pct50)) %>%
+  dplyr::slice(6) %>%
+  mutate(color = case_when(party == "afd" ~ "#00A0E2",
+                           party == "cdu" ~ "black",
+                           party == "fdp" ~ "#FEB900",
+                           party == "gruene" ~ "#19A229",
+                           party == "linke" ~ "#BE3075",
+                           party == "spd" ~ "red"),
+         party_abbr = case_when(party == "afd" ~ "AfD",
+                                party == "cdu" & state == "Bayern" ~ "CSU",
+                                party == "cdu" & state != "Bayern" ~ "CDU",
+                                party == "fdp" ~ "FDP",
+                                party == "gruene" ~ "Greens",
+                                party == "linke" ~ "Linke",
+                                party == "spd" ~ "SPD")) %>%
+  rename_with(function(name) paste0(name, "_6"), .cols = -state)
 
 # Read in shapefile
 state_shp <- readOGR("data/shapes/germany_states.shp") %>%
@@ -46,12 +154,53 @@ state_shp <- readOGR("data/shapes/germany_states.shp") %>%
          alpha = sqrt((largest_pct - 0.2) / 0.2)) %>%
   
   # Label-related things
+  left_join(state_summary_stats_1, by = c("name_1" = "state")) %>%
+  left_join(state_summary_stats_2, by = c("name_1" = "state")) %>%
+  left_join(state_summary_stats_3, by = c("name_1" = "state")) %>%
+  left_join(state_summary_stats_4, by = c("name_1" = "state")) %>%
+  left_join(state_summary_stats_5, by = c("name_1" = "state")) %>%
+  left_join(state_summary_stats_6, by = c("name_1" = "state")) %>%
   mutate(english_name = case_when(is.na(varname_1) ~ name_1,
                                   varname_1 == "<Null>" ~ name_1,
                                   !is.na(varname_1) ~ varname_1),
          mouseover_label = case_when(english_name != name_1 ~ paste0(name_1, " (", english_name, ")"),
-                                     english_name == name_1 ~ name_1))
+                                     english_name == name_1 ~ name_1),
+         popup_label = paste0("<H3><b><u>", name_1, "</u></b></H3>
+                              <b><i>Projected vote (90% CI)</i></b><br>
+                              <font color = ", color_1, "><b>", party_abbr_1, "</b></font>: <b><font color =", color_1, ">", 
+                                percent(vote_pct50_1, accuracy = 0.1), "</font></b> (", percent(vote_pct05_1, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_1, accuracy = 0.1), ")<br>
+                              <font color = ", color_2, "><b>", party_abbr_2, "</b></font>: <b><font color =", color_2, ">", 
+                                percent(vote_pct50_2, accuracy = 0.1), "</font></b> (", percent(vote_pct05_2, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_2, accuracy = 0.1), ")<br>
+                              <font color = ", color_3, "><b>", party_abbr_3, "</b></font>: <b><font color =", color_3, ">", 
+                                percent(vote_pct50_3, accuracy = 0.1), "</font></b> (", percent(vote_pct05_3, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_3, accuracy = 0.1), ")<br>
+                              <font color = ", color_4, "><b>", party_abbr_4, "</b></font>: <b><font color =", color_4, ">", 
+                                percent(vote_pct50_4, accuracy = 0.1), "</font></b> (", percent(vote_pct05_4, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_4, accuracy = 0.1), ")<br>
+                              <font color = ", color_5, "><b>", party_abbr_5, "</b></font>: <b><font color =", color_5, ">", 
+                                percent(vote_pct50_5, accuracy = 0.1), "</font></b> (", percent(vote_pct05_5, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_5, accuracy = 0.1), ")<br>
+                              <font color = ", color_6, "><b>", party_abbr_6, "</b></font>: <b><font color =", color_6, ">", 
+                                percent(vote_pct50_6, accuracy = 0.1), "</font></b> (", percent(vote_pct05_6, accuracy = 0.1), " – ", 
+                                percent(vote_pct95_6, accuracy = 0.1), ")<br>
+                              <br>
+                              <b><i>Projected seats (90% CI)</i></b><br>
+                              <font color = ", color_1, "><b>", party_abbr_1, "</b></font>: <b><font color =", color_1, ">", seats_pct50_1, 
+                                "</font></b> seats (", seats_pct05_1, " – ", seats_pct95_1, ")<br>
+                              <font color = ", color_2, "><b>", party_abbr_2, "</b></font>: <b><font color =", color_2, ">", seats_pct50_2, 
+                                "</font></b> seats (", seats_pct05_2, " – ", seats_pct95_2, ")<br>
+                              <font color = ", color_3, "><b>", party_abbr_3, "</b></font>: <b><font color =", color_3, ">", seats_pct50_3, 
+                                "</font></b> seats (", seats_pct05_3, " – ", seats_pct95_3, ")<br>
+                              <font color = ", color_4, "><b>", party_abbr_4, "</b></font>: <b><font color =", color_4, ">", seats_pct50_4, 
+                                "</font></b> seats (", seats_pct05_4, " – ", seats_pct95_4, ")<br>
+                              <font color = ", color_5, "><b>", party_abbr_5, "</b></font>: <b><font color =", color_5, ">", seats_pct50_5, 
+                                "</font></b> seats (", seats_pct05_5, " – ", seats_pct95_5, ")<br>
+                              <font color = ", color_6, "><b>", party_abbr_6, "</b></font>: <b><font color =", color_6, ">", seats_pct50_6, 
+                                "</font></b> seats (", seats_pct05_6, " – ", seats_pct95_6, ")"))
 
 leaflet(state_shp) %>%
   addTiles() %>%
-  addPolygons(color = "black", weight = 1, opacity = 1, fill = TRUE, fillColor = ~color, fillOpacity = ~alpha, label = ~mouseover_label)
+  addPolygons(color = "black", weight = 1, opacity = 1, fill = TRUE, fillColor = ~color, fillOpacity = ~alpha, label = ~mouseover_label,
+              popup = ~popup_label, highlightOptions = highlightOptions(color = "black", weight = 4, bringToFront = TRUE, opacity = 1))
