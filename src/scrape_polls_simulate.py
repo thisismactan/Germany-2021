9de0a2abcf1c21920e433c9c5e64d864f4235c81
thisismactan <mactan@gmail.com>
2021-08-11 10:09:31 -0400

    fixed scraper since apparently Wikipedia tracks the Freie Waehler now

--- src/scrape_polls_simulate.py
+++ src/scrape_polls_simulate.py
@@ -17,11 +17,9 @@ polls_2021 = pd.DataFrame()
 for i in range(5):
     polls_2021 = polls_2021.append(poll_tables[i])
 
-polls_2021 = polls_2021.iloc[:, 0:12].copy()
-
 # Rename columns
-polls_2021.columns = ['pollster', 'dates', 'n', 'abs', 'cdu', 'spd', 'afd',
-                      'fdp', 'linke', 'gruene', 'other', 'lead']
+polls_2021.columns = ['abs', 'afd', 'fdp', 'fw', 'dates', 'gruene', 'lead', 
+                      'linke', 'other', 'pollster', 'spd', 'n', 'cdu']
 
 polls_2021 = polls_2021[['pollster', 'dates', 'n', 'abs', 'cdu', 'spd',
                          'afd', 'fdp', 'linke', 'gruene', 'other', 'lead']]
